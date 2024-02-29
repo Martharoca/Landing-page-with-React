d886640630e5163e22a0743d21f4786a042b361f
Marta Haro <143503919+Martharoca@users.noreply.github.com>
2024-02-29 11:50:48 +0000

    img

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,14 +2,23 @@ import React from "react";
 
 function Card() {
     return (
-    <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title text-center">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div className="Container my-3" id="card">
+			<div class="row row-cols-1 row-cols-lg-4 g-4 px-5">
+				<div class="col">
+					<div class="card h-100">
+                    <img src="src/js/component/card1.jpg" class="card-img-top"/>
+						<div class="card-body bg-">
+							<h5 class="card-title d-flex justify-content-center">Card title</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+						<div class="card-footer d-flex justify-content-center">
+							<small class="btn btn-primary text-body-secondary">Find out more!</small>
+						</div>
+					</div>
+				</div>
+            </div>
     </div>
-  </div>
   );
 };
 
